aca69d8459265da406de743145e8a8c34b1ce573
hktywong62 <txkyxuwong62@gmail.com>
2023-09-21 22:59:59 +0800

    Week 2  programs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number?  ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessNumber = -1;    
+        
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber == guessNumber)
+            {
+                Console.WriteLine("You guess it!");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                 Console.WriteLine("Higher");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        // set up a number list until the user inputs 0
+        int userNum = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (userNum !=0)
+        {
+            Console.Write("Enter number: ");
+            string response = Console.ReadLine();
+            userNum = int.Parse(response);
+
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+        }
+
+        // Core #1 : Compute the sum of the numbers in the list.
+        int sum = 0;
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}"); 
+
+        // Core #2: Compute the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        // Core #3: Find the max
+        int max = numbers[0];
+        foreach(int number in numbers)
+        {
+            if (number > max)
+            {
+                 max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is {max}");
     }
 }
